7e27e5252314a5f9d5521a96add7f45fa04bb238
Robert Röck <robert.roeck@robotron.de>
2020-10-27 12:33:44 +0100

    add zebra scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const EB: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zebra-tutorial';
+  scanData: string;
+
+  ngOnInit() {
+
+    console.log('FUNCTION:', EB);
+  }
+
+  fnBarcodeScanned = (scan) => {
+    const element = document.getElementById('scanData') as HTMLInputElement;
+    console.log('BARCODE:', scan);
+    this.scanData = 'barcode: ' + scan.data;
+    element.value = scan.data + ' foobar';
+  }
+
+  fnScanEnable = () => {
+    console.log('ENABLE');
+    const element = document.getElementById('scanData') as HTMLInputElement;
+    element.value = 'init';
+    EB.Barcode.enable({allDecoders: true}, this.fnBarcodeScanned);
+    element.value = 'init 2 - enabled: press HW trigger to capture.';
+    this.scanData = 'enabled: press HW trigger to capture.';
+  }
+
+  fnScanDisable = () => {
+    const element = document.getElementById('scanData') as HTMLInputElement;
+    console.log('DISABLE');
+    EB.Barcode.disable();
+    this.scanData = 'disabled: press "enable" to scan.';
+    element.value = 'disabled: press "enable" to scan.';
+  }
 }
